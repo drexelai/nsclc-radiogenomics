4af883056e1df305688dbbd7c55055b25621610c
ethan <57536747+ethanmoyer@users.noreply.github.com>
2021-10-12 16:28:35 -0400

    add radiogenomics feature functions

--- features.py
+++ features.py
@@ -3,85 +3,76 @@ import numpy as np
 import os
 import pydicom
 import matplotlib.pyplot as plt
+import gzip
 
 from lungmask import mask
 import SimpleITK as sitk
-
-def caclVolumne(pixel_array):
-	pass
+import radiomics
 
 
-def calcSurfaceArea(pixel_array):
-	pass
+def calcFirstOrderFeatures(segmentation, pixel_array):
+	extractor = radiomics.firstorder.RadiomicsFirstOrder(pixel_array, segmentation)
+	return np.array([v for _, v in extractor.execute().items()])
 
 
-def calcSphericity(pixel_array):
-	pass
+def calcShapeBased3DFeatures(segmentation, pixel_array):
+	extractor = radiomics.shape.RadiomicsShape(pixel_array, segmentation)
+	return np.array([v for _, v in extractor.execute().items()])
 
 
-def calcMean(pixel_array):
-	pass
+def calcShapeBased2DFeatures(segmentation, pixel_array):
+	extractor = radiomics.shape2D.RadiomicsShape2D(pixel_array, segmentation, force2D=True)
+	return np.array([v for _, v in extractor.execute().items()])
 
 
-def calcSRD(pixel_array):
-	pass
-	
-
-def calcPercentiles(pixel_array):
-	pass
+def calcGLCMFeatures(segmentation, pixel_array):
+	extractor = radiomics.glcm.RadiomicsGLCM(pixel_array, segmentation)
+	return np.array([v for _, v in extractor.execute().items()])
 
 
-def calcKurtosis(pixel_array):
-	pass
+def calcGLRLMFeatures(segmentation, pixel_array):
+	extractor = radiomics.glrlm.RadiomicsGLRLM(pixel_array, segmentation)
+	return np.array([v for _, v in extractor.execute().items()])
 
 
-def calcSkewness(pixel_array):
-	pass
+def calcGLSZMFeatures(segmentation, pixel_array):
+	extractor = radiomics.glszm.RadiomicsGLSZM(pixel_array, segmentation)
+	return np.array([v for _, v in extractor.execute().items()])
 
 
-def calcFrequency(pixel_array):
-	pass
+def calcNGTDMFeatures(segmentation, pixel_array):
+	extractor = radiomics.ngtdm.RadiomicsNGTDM(pixel_array, segmentation)
+	return np.array([v for _, v in extractor.execute().items()])
 
 
-def caclVesselDesnity(pixel_array):
-	pass
+def calcGLDMFeatures(segmentation, pixel_array):
+	extractor = radiomics.gldm.RadiomicsGLDM(pixel_array, segmentation)
+	return np.array([v for _, v in extractor.execute().items()])
 
 
-def calcAutoCorrelatioin(pixel_array):
-	pass
 
-
-def calcClusterProminence(pixel_array):
-	pass
-
-
-def calcDifferenceVariance(pixel_array):
-	pass
-
-
-def calcHomogenity(pixel_array):
-	pass
+def calcFeatures(segmentation, pixel_array):
+	return np.concatenate([calcFirstOrderFeatures(segmentation, pixel_array), calcShapeBased3DFeatures(segmentation, pixel_array), calcShapeBased2DFeatures(segmentation, pixel_array), calcGLCMFeatures(segmentation, pixel_array), calcGLRLMFeatures(segmentation, pixel_array), calcGLSZMFeatures(segmentation, pixel_array), calcNGTDMFeatures(segmentation, pixel_array), calcGLDMFeatures(segmentation, pixel_array)])
 
 
 def segmentLungs(imagepath):
 	# imagepath = '/Volumes/Extended/drexelai/radiogenomics/NSCLC/NSCLC_Radiogenomics-6-1-21 Version 4/manifest-1622561851074/NSCLC Radiogenomics/AMC-001/1.3.6.1.4.1.14519.5.2.1.4334.1501.227933499470131058806289574760/1.3.6.1.4.1.14519.5.2.1.4334.1501.131836349235351218393791897864/1-092.dcm'
 
-	segimagepath = os.path.join(os.path.split(imagepath)[0], 'seg_' + os.path.split(imagepath)[1].split('.')[0]) + '.npy'
+	segimagepath = os.path.join(os.path.split(imagepath)[0], 'seg_' + os.path.split(imagepath)[1].split('.')[0]) + '.dcm'
 
 	if os.path.isfile(segimagepath):
-		segmentation = np.load(segimagepath)
+		segmentation = sitk.ReadImage(segimagepath)
 	else:
 		pixel_array = sitk.ReadImage(imagepath)
-		segmentation = mask.apply(pixel_array)[0]
-		np.save(segimagepath, segmentation)
+		segmentation = mask.apply(pixel_array)
+		# segmentation_ = segmentation
+		segmentation = sitk.GetImageFromArray(segmentation)  
+		sitk.WriteImage(segmentation, segimagepath)
+		# np.save(segimagepath, segmentation)
 	# plt.imshow(pydicom.dcmread(imagepath).pixel_array, cmap=plt.cm.bone)
 	# plt.imshow(segmentation, cmap='jet', alpha=0.2)
 	# plt.show()
 	return segmentation
-
-
-def calcFeatures(segmentation, pixel_array):
-	return [caclVolumne(pixel_array_seg), calcSurfaceArea(pixel_array_seg), calcSphericity(pixel_array_seg), calcMean(pixel_array_seg), calcSRD(pixel_array_seg), calcPrecentiles(pixel_array_seg), calcPercentiles(pixel_array_seg), calcKurtosis(pixel_array_seg), calcSkewness(pixel_array_seg), calcFrequency(pixel_array_seg), caclVesselDesnity(pixel_array_seg), calcAutoCorrelatioin(pixel_array_seg), calcClusterProminence(pixel_array_seg), calcDifferenceVariance(pixel_array_seg), calcHomogenity(pixel_array_seg)]
 
 
 def main(rootdir):
@@ -107,11 +98,16 @@ def main(rootdir):
 					if image == '.DS_Store':
 						continue
 					imagepath = os.path.join(study2dir, image)
-					# TODO: figure out which images to actually encode for features
 					segmentation = segmentLungs(imagepath)
 					pixel_array = pydicom.dcmread(imagepath).pixel_array
 
 					results[patientid] = calcFeatures(segmentation, pixel_array)
+
+
+def preprocessRNASeq(rnaseqloc):
+	filename = '/Volumes/Extended/drexelai/radiogenomics/NSCLC/GSE103584_R01_NSCLC_RNAseq.txt.gz'
+
+	data = pd.read_csv(gzip.open(filename), sep='\t', index_col=0)
 
 if __name__ == '__main__':
 	rootdir = '/Volumes/Extended/drexelai/radiogenomics/NSCLC/NSCLC_Radiogenomics-6-1-21 Version 4/manifest-1622561851074'
