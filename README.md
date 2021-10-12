2ac444dbac5cc581ffb7968acc3826dacc8038d3
ethan <57536747+ethanmoyer@users.noreply.github.com>
2021-10-12 07:56:44 -0400

    add unet support

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # nsclc-radiogenomics
 Drexel AI 2021 Fall
 
-# TASKS
+# Setup
+
+Run the following in the root directory of this project in order to install all dependencies. 
+
+``` bash
+pip install -r requirements.txt
+```
+
+# Tasks
 
 - [x] data set
   - https://wiki.cancerimagingarchive.net/display/Public/NSCLC+Radiogenomics
@@ -9,6 +17,7 @@ Drexel AI 2021 Fall
   - https://www.overleaf.com/4826185589tjyzkynzsbfb
 - [] preprocess clinical features: Alex
 - [] preprocess genomic features: Ethan
+- [x] segment images: Ethan
 - [] preprocess imaging features: Ethan, Alex, Satvik
   - https://theaisummer.com/medical-image-python/
 - [] calculate correlation, clustering (PCA, hierachial clustering, distributions), and exclude highly correlative features
--- features.py
+++ features.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import os
 import pydicom
+import matplotlib.pyplot as plt
+
+from lungmask import mask
+import SimpleITK as sitk
 
 def caclVolumne(pixel_array):
 	pass
@@ -21,11 +25,7 @@ def calcMean(pixel_array):
 
 def calcSRD(pixel_array):
 	pass
-
-
-def calcPrecentiles(pixel_array):
-	pass
-
+	
 
 def calcPercentiles(pixel_array):
 	pass
@@ -63,34 +63,24 @@ def calcHomogenity(pixel_array):
 	pass
 
 
-def segment_lungs(pixel_array):
-	pass
+def segmentLungs(imagepath):
+	# imagepath = '/Volumes/Extended/drexelai/radiogenomics/NSCLC/NSCLC_Radiogenomics-6-1-21 Version 4/manifest-1622561851074/NSCLC Radiogenomics/AMC-001/1.3.6.1.4.1.14519.5.2.1.4334.1501.227933499470131058806289574760/1.3.6.1.4.1.14519.5.2.1.4334.1501.131836349235351218393791897864/1-092.dcm'
+
+	segimagepath = os.path.join(os.path.split(imagepath)[0], 'seg_' + os.path.split(imagepath)[1].split('.')[0]) + '.npy'
+
+	if os.path.isfile(segimagepath):
+		segmentation = np.load(segimagepath)
+	else:
+		pixel_array = sitk.ReadImage(imagepath)
+		segmentation = mask.apply(pixel_array)[0]
+		np.save(segimagepath, segmentation)
+	# plt.imshow(pydicom.dcmread(imagepath).pixel_array, cmap=plt.cm.bone)
+	# plt.imshow(segmentation, cmap='jet', alpha=0.2)
+	# plt.show()
+	return segmentation
 
 
-def find_pix_dim(ct_img):
-    """
-    Get the pixdim of the CT image.
-    A general solution that gets the pixdim indicated from the image dimensions. From the last 2 image dimensions, we get their pixel dimension.
-    Args:
-        ct_img: nib image
-
-    Returns: List of the 2 pixel dimensions
-    """
-    pix_dim = ct_img.header["pixdim"] # example [1,2,1.5,1,1]
-    dim = ct_img.header["dim"] # example [1,512,512,1,1]
-    max_indx = np.argmax(dim)
-    pixdimX = pix_dim[max_indx]
-    dim = np.delete(dim, max_indx)
-    pix_dim = np.delete(pix_dim, max_indx)
-    max_indy = np.argmax(dim)
-    pixdimY = pix_dim[max_indy]
-    return [pixdimX, pixdimY] # example [2, 1.5]
-    
-
-def calcFeatures(pixel_array):
-
-	pixel_array_seg = segment_lungs(pixel_array)
-
+def calcFeatures(segmentation, pixel_array):
 	return [caclVolumne(pixel_array_seg), calcSurfaceArea(pixel_array_seg), calcSphericity(pixel_array_seg), calcMean(pixel_array_seg), calcSRD(pixel_array_seg), calcPrecentiles(pixel_array_seg), calcPercentiles(pixel_array_seg), calcKurtosis(pixel_array_seg), calcSkewness(pixel_array_seg), calcFrequency(pixel_array_seg), caclVesselDesnity(pixel_array_seg), calcAutoCorrelatioin(pixel_array_seg), calcClusterProminence(pixel_array_seg), calcDifferenceVariance(pixel_array_seg), calcHomogenity(pixel_array_seg)]
 
 
@@ -118,7 +108,10 @@ def main(rootdir):
 						continue
 					imagepath = os.path.join(study2dir, image)
 					# TODO: figure out which images to actually encode for features
-					results[patientid] = calcFeatures(pydicom.dcmread(imagepath).pixel_array)
+					segmentation = segmentLungs(imagepath)
+					pixel_array = pydicom.dcmread(imagepath).pixel_array
+
+					results[patientid] = calcFeatures(segmentation, pixel_array)
 
 if __name__ == '__main__':
 	rootdir = '/Volumes/Extended/drexelai/radiogenomics/NSCLC/NSCLC_Radiogenomics-6-1-21 Version 4/manifest-1622561851074'
